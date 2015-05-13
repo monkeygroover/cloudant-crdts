ae2360763267be91a28fa7053b19357118d46a18
rilakkuma <richard.bowker@monkeygroover.com>
2015-05-13 23:06:17 +0100

    build against local snapshot of couchdb-scala

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,21 +4,15 @@
 
 import com.ibm.couchdb._
 import com.typesafe.config.ConfigFactory
+import crdts.Vote
 
 import scalaz._
 
 object Main extends App {
 
-  // Define a simple case class to represent our data model
-  case class Person(name: String, age: Int)
-
-  // Define some sample data
-  val alice = Person("Alice", 25)
-  val bob   = Person("Bob", 30)
-  val carl  = Person("Carl", 20)
-
   // Define a type mapping used to transform class names into the doc kind
-  val typeMapping = TypeMapping(classOf[Person] -> "Person")
+  val typeMapping = TypeMapping(
+    classOf[Vote] -> "Vote")
 
   val config = ConfigFactory.load()
   val host = config.getString("cloudant.host")
@@ -40,23 +34,40 @@ object Main extends App {
   // Get an instance of the DB API by name and type mapping
   val db  = cloudant.db(dbName, typeMapping)
 
+  val pollId = "dsfdsfsdf"
+
+  val pollView = CouchView(map =
+    """
+      |function(doc){
+      |   if(doc.doc.pollId){
+      |       emit(doc.doc.pollId, doc.doc);
+      |   }
+      |}
+    """.stripMargin)
+
+  val designDoc = CouchDesign(
+    name  = "test-design",
+    views = Map("poll-view" -> pollView))
+
+  val pollViewQuery = db.query.view[String, Vote]("test-design", "poll-view").get
+
+  val votes = Vote("poll1", 1) :: Vote("poll2", 2) :: Vote("poll1", 3) :: Vote("poll2", 2) :: Vote("poll2", 1) :: Vote("poll1", 7) :: Nil
+
+
   val actions = for {
-  // Delete the database or ignore the error if it doesn't exist
     _ <- cloudant.dbs.delete(dbName).ignoreError
-    // Create a new database
     _ <- cloudant.dbs.create(dbName)
-    // Insert documents into the database
-    _ <- db.docs.createMany(Seq(alice, bob, carl))
-    // Retrieve all documents from the database and unserialize to Person
-    docs <- db.docs.getMany.queryIncludeDocs[Person]
-  } yield docs.getDocsData
+    _ <- db.design.create(designDoc)
+    _ <- db.docs.createMany(votes)
+    result <- pollViewQuery.queryIncludeDocs[Vote]("poll1" :: Nil)
+  } yield result
 
   // Execute the actions and process the result
   actions.attemptRun match {
     // In case of an error (left side of Either), print it
     case -\/(e) => println(e)
     // In case of a success (right side of Either), print each object
-    case \/-(a) => a.map(println(_))
+    case \/-(a) => println(a)
+    case x => println(x)
   }
-
 }
---
+++ src/main/scala/crdts/package.scala
@@ -0,0 +1,8 @@
+/**
+ * Created by rilakkuma on 05/05/2015.
+ */
+package object crdts {
+
+  case class Vote(pollId: String, value: Int)
+
+}
